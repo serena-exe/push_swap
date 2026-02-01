de260d76b2d374199832c6e3e3155971b80a519f
Séréna Bavarois <serenabavarois@Mac.lan>
2026-02-01 11:56:41 +0100

    rm .git

---
+++ ft_libft/ft_atol.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebavaro <sebavaro@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 11:56:19 by sebavaro          #+#    #+#             */
+/*   Updated: 2026/02/01 11:56:31 by sebavaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *str)
+{
+    int		i;
+    int		sign;
+    long	result;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (result * sign);
+}
